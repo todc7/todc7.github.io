dd37f9840fe68cd831e653bb34b71062764fd02d
todc7 <typemultitask@gmail.com>
2024-10-04 13:37:40 -0600

    Add files via upload

---
+++ inc/config.php
@@ -0,0 +1,10 @@
+<?php
+$CONF_TIMEOUT_OFFLINE               = 120;
+$CONF_TIMEOUT_DEAD                  = 259200;
+$CONF_DB_HOST                       = "127.0.0.1";
+$CONF_DB_NAME                       = "panel";
+$CONF_DB_USER                       = "root";
+$CONF_DB_PASS                       = "7112";
+$CONF_PANEL_USER                    = "todc";
+$CONF_PANEL_PASS                    = "e0746d7f5698dbe5b5f57ecf1064c1c5ca6dc74b0e1029e47b9eb0e7823682368b04fa550a92b06a1ead2ed6dffc010f236f88fdd3abf6c367f6ea22ce120acb";
+?>
